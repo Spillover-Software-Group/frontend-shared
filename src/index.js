feb8e7374e88157959926c72c574b7f2374f2359
Felipe Zavan <felipe@zavan.me>
2025-12-23 11:51:45 +0000

    Export ui components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Provider from './Provider';
-import { AITextHelpers} from "./ai/AITextHelpers";
+import { AITextHelpers } from "./ai/AITextHelpers";
 import { AITextHelpersButton } from "./ai/AITextHelpersButton";
 import { CustomerReports } from "./pages/CustomerReports";
 
 import './styles.css';
 
 export { Provider as SpilloverProvider, AITextHelpers, AITextHelpersButton, CustomerReports };
+export * from "./ui";
---
+++ src/ui/index.js
@@ -0,0 +1,45 @@
+export { Button } from "./Button";
+
+export {
+  Description,
+  FieldError,
+  FieldGroup,
+  Input,
+  Label,
+  fieldBorderStyles,
+  fieldGroupStyles,
+} from "./Field";
+export { Form } from "./Form";
+export { TextField } from "./TextField";
+export { Select, SelectItem, SelectSection } from "./Select";
+export { ComboBox, ComboBoxItem, ComboBoxSection } from "./ComboBox";
+export {
+  ListBox,
+  ListBoxItem,
+  DropdownItem,
+  DropdownSection,
+  itemStyles,
+  dropdownItemStyles,
+} from "./ListBox";
+
+export { Calendar, CalendarHeader, CalendarGridHeader } from "./Calendar";
+export { DateField, DateInput } from "./DateField";
+export { DateRangePicker } from "./DateRangePicker";
+export { RangeCalendar } from "./RangeCalendar";
+export {
+  PresetDateRangeSelector,
+  usePresetDateRangeSelector,
+} from "./PresetDateRangeSelector";
+
+export { Dialog, Heading } from "./Dialog";
+export { Modal } from "./Modal";
+export { Popover } from "./Popover";
+export { Section } from "./Section";
+
+export { LoadingSpinner } from "./LoadingSpinner";
+
+export { MiniApp, Screen } from "./MiniApp";
+export { default as MiniAppHeader } from "./MiniApp/Header";
+export { default as HomeScreen } from "./MiniApp/HomeScreen";
+export { default as MiniAppScreens } from "./MiniApp/Screens";
+export { MiniAppProvider, useMiniApp } from "./MiniApp/useMiniApp";
